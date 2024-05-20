4d3c88debcf90f1eaf887ccc23f215e9c65b0539
Stephen Nyabuto <bossteve98@gmail.com>
2024-05-20 15:16:30 +0300

    commit initial files

---
+++ passenger counter app/index.js
@@ -0,0 +1,72 @@
+
+// let countEl = document.getElementById("count-el")
+// let saveEl = document.getElementById("save-el")
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countstr = count + " - "
+//     saveEl.textContent += countstr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+
+//VARIABLE PRACTISE
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+// let firstName = "Stephen "
+// let lastName =  "Nyabuto"
+
+// let fullName = firstName + lastName
+
+// console.log(fullName)
+
+
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+// function fullGreeting(){
+//     console.log(greeting +", "  + name + "!" )
+// }
+// fullGreeting()
+
+// function fullGreeting(){
+//     finalGreeting = greeting +"," + " " + name + "!"
+//     console.log(finalGreeting)
+// }
+// fullGreeting()
+
+
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+function add3Points(){
+    myPoints +=3
+    
+}
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+
+
+// function remove1POint
+function remove1Point(){
+    myPoints -= 1
+}
+
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
